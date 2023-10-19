b7354f6d5eba318bc66fb711e23119235ac74002
Ryztard <72693058+Ryztard@users.noreply.github.com>
2023-10-19 11:56:03 +0200

    Update page.js
    Prøvde å lage post method

--- SendLærer/app/animals/page.js
+++ SendLærer/app/animals/page.js
@@ -7,19 +7,34 @@ import { useEffect, useState } from "react";
 
 const Animals = () => {
     const [catfacts, setCatFacts] = useState([])
+    const [fact, setFact] = useState([])
 
-const getCatFacts = async () => {
+    const getCatFacts = async () => {
+        try {
+            const response = await axios.get("https://cat-fact.herokuapp.com/facts");
+            console.log(response);
+
+            if (response?.data?.success) {
+                setCatFacts(response.data.data);
+            }
+        } catch (error) {
+            console.log(error?.response?.data);
+        }
+    };
+
+const postCatFact = async () => {
     try {
-    const response = await axios.get("https://cat-fact.herokuapp.com/facts")
-    console.log(response)
+        const response = await axios.post("https://cat-fact.herokuapp.com/facts", {
+            text: fact,
+        });
 
-    if (response?.data?.success) {
-        setCatFacts(response.data.data)
+        if (response?.data?.status) {
+            getCatFacts();
+        }
+    } catch (error) {
+        console.log(error?.response?.data);
     }
-}   catch(error) {
-    console.log(error?.response?.data)
-    }
-}
+};
 
 useEffect(() => {
     getCatFacts();
@@ -31,11 +46,18 @@ useEffect(() => {
             <title>Animals</title>
         </Head>
             <h1>ANIMALS</h1>
-            <section>                      
-                {catfacts.map((fact) => (
-                    <li key={fact._id}>{fact.text}</li>
-                ))}
-                {JSON.stringify(catfacts)}
+            <section>
+                <input
+                    type="text"
+                    value={fact}
+                    onChange={(e) => setFact(e.target.value)}
+                />
+                <button onClick={postCatFact}>Post Cat Fact</button>
+                <ul>
+                    {catfacts.map((catFact) => (
+                        <li key={catFact._id}>{catFact.text}</li>
+                    ))}
+                </ul>
             </section>
             
         </>
